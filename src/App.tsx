f12631c01d249bd21c9b4f6b0a1a5cb18e87d293
MatthewBart <MatthewCBartley@gmail.com>
2022-11-22 15:59:58 +0000

    Basic Functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const[colour, setColour]=useState("");
+  const[answers,setAnswers]=useState<string[]>([]);
+  const[isIncorrect,setIncorrect]= useState(false);
+
+  const getRandomColour=()=>{
+
+    const digits =
+    ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+  
+    const colour = new Array(6)
+        .fill("")
+        .map(()=>digits[Math.floor(Math.random() * digits.length)])
+        .join("");
+        return `#${colour}`;
+
+  }
+
+  useEffect(() => {
+    const correctColour= getRandomColour();
+    setAnswers([correctColour,getRandomColour(),getRandomColour()].sort(()=>0.5-Math.random()));
+    setColour(correctColour);
+  }, [])
+
+  function handleClick(answer:string){
+    if(answer==colour){
+      setIncorrect(false);
+    }
+    else{
+      setIncorrect(true);
+    }
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="column">
+        {isIncorrect && <div>Wrong Answer</div>}
+        <div className="colour-picker" style={{background: colour}}></div>
+          {answers.map((answers)=>(
+          <button
+            onClick={()=>handleClick(answers)} key={answers}>{answers}</button>
+          ))}
+      </div>
     </div>
   );
 }
